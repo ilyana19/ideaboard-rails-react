f4c58230a4e4b698a9d3a77b318cfd661995aa3c
ilyana19 <aries_2152@hotmail.com>
2018-12-11 18:23:51 -0500

    fixed spelling errors, completed CRUD

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -3,25 +3,24 @@ import axios from 'axios'
 
 class IdeaForm extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
+		super(props)
+		this.state = {
       title: this.props.idea.title,
       body: this.props.idea.body
-    }
-  }
+		}
+	}
 
   handleInput = (e) => {
-    this.setState({
-      [e.target.name]: [e.target.value]
-    })
+    this.props.resetNotification()
+    this.setState({[e.target.name]: e.target.value})
   }
 
   handleBlur = () => {
-    const idea = {title: this.state.title, body: this.state.body}
-
+    const idea = {title: this.state.title, body: this.state.body }
     axios.put(`http://localhost:3001/api/v1/ideas/${this.props.idea.id}`, {idea: idea})
     .then(response => {
       console.log(response)
+      this.props.updateIdea(response.data)
     })
     .catch(error => console.log(error))
   }
@@ -29,20 +28,10 @@ class IdeaForm extends Component {
   render() {
     return (
       <div className="tile">
-        <form onBlur={this.handleBlur}>
-          <input className="input"
-            type="text" name="title"
-            placeholder="Enter a title"
-            value={this.state.title}
-            onChange={this.handleInput} />
-
-          <textarea className="input"
-            name="body"
-            placeholder="Describe your idea"
-            value={this.state.body}
-            onChange={this.handleInput}>
-          </textarea>
-        </form>
+      	<form onBlur={this.handleBlur} >
+					<input className='input' type="text" name="title" placeholder='Enter a Title' value={this.state.title} onChange={this.handleInput} ref={this.props.titleRef} />
+					<textarea className='input' name="body" placeholder='Describe your idea' value={this.state.body} onChange={this.handleInput}></textarea>
+      	</form>
       </div>
     )
   }
